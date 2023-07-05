06213519fc832bf5a25e0e818c6bcef9b809411a
xBeatrice <betyyyy56@gmail.com>
2023-07-05 15:04:20 +0300

    Fix page navigation

--- projectsStorageService.js
+++ projectsStorageService.js
@@ -5,218 +5,285 @@ const firebaseApp = firebase.initializeApp({
   storageBucket: "invoicemasters.appspot.com",
   messagingSenderId: "114175730381",
   appId: "1:114175730381:web:fc9365bd8e0f0360aa7f84",
-  measurementId: "G-YFVW8PFMNM"
+  measurementId: "G-YFVW8PFMNM",
 });
 
- const db = firebaseApp.firestore();
- const auth = firebaseApp.auth();
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
 
+// AUTHENTIFICATION SERVICE
 
-// AUTHENTIFICATION SERVICE 
+const register = () => {
+  const email = document.getElementById("newAccEmail").value;
+  const password = document.getElementById("newAccPassword").value;
 
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((res) => {
+      console.log(res.user);
+      location.href = "profilePage.html";
+    })
+    .catch((err) => {
+      alert(
+        "Adresa de mail nu exista sau deja a fost creat un cont cu aceasta adresa."
+      );
+      console.log(err.code);
+      console.log(err.message);
+    });
+};
 
-const register=()=>{
-	const email=document.getElementById('newAccEmail').value
-	const password=document.getElementById('newAccPassword').value
+let currentUser = {};
 
-	auth.createUserWithEmailAndPassword(email, password)
-	.then((res) => {
-		console.log(res.user)
-	})
-	.catch((err) => {
-		alert("Adresa de mail nu exista sau deja a fost creat un cont cu aceasta adresa.")
-		console.log(err.code)
-		console.log(err.message)
-	})
-}
+const signIn = () => {
+  const email = document.getElementById("signInEmail").value;
+  const password = document.getElementById("signInPassword").value;
 
-let currentUser = {}
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((res) => {
+      console.log(res.user);
 
-const signIn=()=>{
-	const email=document.getElementById('signInEmail').value
-	const password=document.getElementById('signInPassword').value
+      currentUser = res.user;
+      location.href = "profilePage.html";
+    })
+    .catch((err) => {
+      alert("Email sau parola gresita. Incearca din nou!");
+      console.log(err.code);
+      console.log(err.message);
+    });
+};
 
-	auth.signInWithEmailAndPassword(email, password)
-	.then((res) => {
-		console.log(res.user)
+const logOut = () => {
+  auth
+    .signOut()
+    .then(() => {
+      console.log("Logged out");
+      location.href = "authentificationPage.html";
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
 
-		currentUser = res.user
-		
-	})
-	.catch((err) => {
-		alert("Email sau parola gresita. Incearca din nou!")
-		console.log(err.code)
-		console.log(err.message)
-	})
-}
+const resetPassword = () => {
+  const email = currentUser;
 
-const logOut=()=>{
-	auth.signOut()
-	.then(()=>{
-		console.log('Logged out')
-		location.href='authentificationPage.html'
-	})
-	.catch(error => {
-		console.error(error)
-	})
-}
-
-const resetPassword=()=>{
-	const email=document.getElementById('resetPassUser').value
-
-	auth.sendPasswordResetEmail(email)
-	.then(() => {
-		alert('Emailul de resetare a parolei a fost trimis cu succes.')
-	})
-	.catch(error => {
-		console.error(error)
-	})
-}
+  auth
+    .sendPasswordResetEmail(email)
+    .then(() => {
+      alert("Emailul de resetare a parolei a fost trimis cu succes.");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
 //asign random unique id to users not logged in
 let s4 = () => {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-    }
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+};
 
 // listen for auth status changes
 
-let loggedIn=false
-auth.onAuthStateChanged(user => {
-	if (user) {
-		currentUser = user.email
-		loggedIn=true
-	// } else {
-	// currentUser=s4()+s4()+ '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
- 	}
-})
-
-
+let loggedIn = false;
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    currentUser = user.email;
+    loggedIn = true;
+    // } else {
+    // currentUser=s4()+s4()+ '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  }
+});
 
 //PROJECTS STORAGE SERVICE
 
+let projectId = "";
+const newProject = (name, description, date, currentUser) => {
+  return db.collection("projects").add({
+    name,
+    description,
+    date,
+    currentUser,
+  });
+};
 
-let projectId = ""
- const newProject = (name, description, date, currentUser) => {
- 	return db.collection('projects')
- 	.add({
- 		name,
- 		description,
- 		date,
- 		currentUser
- 	})
-  	
-}
-
-const getProjects = () => db.collection('projects').get()
-
+const getProjects = () => db.collection("projects").get();
 
 const newTemplate = (name, description, date, currentUser, projectId) => {
- 	db.collection('templates')
- 	.add({
- 		name,
- 		description,
- 		date,
- 		currentUser,
- 		projectId
- 	})
- 	.then((queryResult)=> console.log("Document written with ID: ", queryResult.id))
- 	.catch((err) => {
-		console.log(err.code)
-		console.log(err.message)
-	})
-}
+  db.collection("templates")
+    .add({
+      name,
+      description,
+      date,
+      currentUser,
+      projectId,
+    })
+    .then((queryResult) =>
+      console.log("Document written with ID: ", queryResult.id)
+    )
+    .catch((err) => {
+      console.log(err.code);
+      console.log(err.message);
+    });
+};
 
-const newCompany = (companyName, companyAdress, companyVAT, companyIBAN, currentUser) => {
- 	return db.collection('companyInfo')
- 	.add({
- 		companyName, 
- 		companyAdress, 
- 		companyVAT, 
- 		companyIBAN, 
- 		currentUser
- 	})
-  	.then((queryResult)=> console.log("Document written with ID: ", queryResult.id))
-  	.catch((err) => {
-		console.log(err.code)
-		console.log(err.message)
-	})
-}
+const newCompany = (
+  companyName,
+  companyAdress,
+  companyVAT,
+  companyIBAN,
+  currentUser
+) => {
+  return db
+    .collection("companyInfo")
+    .add({
+      companyName,
+      companyAdress,
+      companyVAT,
+      companyIBAN,
+      currentUser,
+    })
+    .then((queryResult) =>
+      console.log("Document written with ID: ", queryResult.id)
+    )
+    .catch((err) => {
+      console.log(err.code);
+      console.log(err.message);
+    });
+};
 
-const newVendor = (vendorName, vendorAdress, vendorVAT, vendorEmail, currentUser) => {
-	return db.collection('vendors')
- 	.add({
- 		vendorName, 
- 		vendorAdress, 
- 		vendorVAT,
- 		vendorEmail, 
- 		currentUser
- 	})
-  	.then((queryResult)=> console.log("Document written with ID: ", queryResult.id))
-  	.catch((err) => {
-		console.log(err.code)
-		console.log(err.message)
-	})
-}
+const newVendor = (
+  vendorName,
+  vendorAdress,
+  vendorVAT,
+  vendorEmail,
+  currentUser
+) => {
+  return db
+    .collection("vendors")
+    .add({
+      vendorName,
+      vendorAdress,
+      vendorVAT,
+      vendorEmail,
+      currentUser,
+    })
+    .then((queryResult) =>
+      console.log("Document written with ID: ", queryResult.id)
+    )
+    .catch((err) => {
+      console.log(err.code);
+      console.log(err.message);
+    });
+};
 
-const newProduct = (productName, productDescription, productPrice, productStock, currentUser) => {
-	return db.collection('products')
- 	.add({
- 		productName, 
- 		productDescription, 
- 		productPrice,
- 		productStock, 
- 		currentUser
- 	})
-  	.then((queryResult)=> console.log("Document written with ID: ", queryResult.id))
-  	.catch((err) => {
-		console.log(err.code)
-		console.log(err.message)
-	})
-}
+const newProduct = (
+  productName,
+  productDescription,
+  productPrice,
+  productStock,
+  currentUser
+) => {
+  return db
+    .collection("products")
+    .add({
+      productName,
+      productDescription,
+      productPrice,
+      productStock,
+      currentUser,
+    })
+    .then((queryResult) =>
+      console.log("Document written with ID: ", queryResult.id)
+    )
+    .catch((err) => {
+      console.log(err.code);
+      console.log(err.message);
+    });
+};
 
-const getProducts = () => db.collection('products').get()
+const getProducts = () => db.collection("products").get();
 
-const getVendors = () => db.collection('vendors').get()
+const getVendors = () => db.collection("vendors").get();
 
-const updateVendor = async(vendorId, name, adress, VAT, email,)=>{
-	
-	db.collection("vendors").doc(vendorId).set({vendorName: name, vendorAdress: adress, vendorVAT: VAT, vendorEmail: email, currentUser: currentUser});
-} 
-const updateProduct = async(productId, name, description, price, stock,)=>{
-	
-	db.collection("products").doc(productId).set({productName: name, productDescription: description, productPrice: price, productStock: stock, currentUser: currentUser});
-} 
-const getCompanyInfo = () => db.collection('companyInfo').get()
+const updateVendor = async (vendorId, name, adress, VAT, email) => {
+  db.collection("vendors").doc(vendorId).set({
+    vendorName: name,
+    vendorAdress: adress,
+    vendorVAT: VAT,
+    vendorEmail: email,
+    currentUser: currentUser,
+  });
+};
+const updateProduct = async (productId, name, description, price, stock) => {
+  db.collection("products").doc(productId).set({
+    productName: name,
+    productDescription: description,
+    productPrice: price,
+    productStock: stock,
+    currentUser: currentUser,
+  });
+};
+const getCompanyInfo = () => db.collection("companyInfo").get();
 
-const updateCompanyInfo = async(companyId, name, adress, VAT, IBAN, currentUser)=>{
-	
-	db.collection("companyInfo").doc(companyId).set({companyName: name, companyAdress: adress, companyVAT: VAT, companyIBAN: IBAN, currentUser: currentUser});
-} 
+const updateCompanyInfo = async (
+  companyId,
+  name,
+  adress,
+  VAT,
+  IBAN,
+  currentUser
+) => {
+  db.collection("companyInfo").doc(companyId).set({
+    companyName: name,
+    companyAdress: adress,
+    companyVAT: VAT,
+    companyIBAN: IBAN,
+    currentUser: currentUser,
+  });
+};
 
-const getTemplates = () => db.collection('templates').get()
+const getTemplates = () => db.collection("templates").get();
 
-const editProjectData = async(projectId, today, newName, newDescription)=>{
-	
-	db.collection("projects").doc(projectId).set({name: newName, description: newDescription, date: today, currentUser: currentUser});
-}
+const editProjectData = async (projectId, today, newName, newDescription) => {
+  db.collection("projects").doc(projectId).set({
+    name: newName,
+    description: newDescription,
+    date: today,
+    currentUser: currentUser,
+  });
+};
 
-const editTemplateData = async(templateId, today, newName, newDescription, projectId)=>{
-	
-	db.collection("templates").doc(templateId).set({name: newName, description: newDescription, date: today, currentUser: currentUser, projectId: projectId});
-}
-const modifyTemplateContent = async(templateId, templateContent)=>{
-	db.collection("templates").doc(templateId).set({content: templateContent}, { merge: true })
-}
+const editTemplateData = async (
+  templateId,
+  today,
+  newName,
+  newDescription,
+  projectId
+) => {
+  db.collection("templates").doc(templateId).set({
+    name: newName,
+    description: newDescription,
+    date: today,
+    currentUser: currentUser,
+    projectId: projectId,
+  });
+};
+const modifyTemplateContent = async (templateId, templateContent) => {
+  db.collection("templates")
+    .doc(templateId)
+    .set({ content: templateContent }, { merge: true });
+};
 const deleteProjectDB = async (projectId) => {
-	db.collection('projects').doc(projectId).delete()
-}
+  db.collection("projects").doc(projectId).delete();
+};
 const deleteTemplateDB = async (templateId) => {
-	db.collection('templates').doc(templateId).delete()
-}
+  db.collection("templates").doc(templateId).delete();
+};
 const deleteVendorData = async (vendorId) => {
-	db.collection('vendors').doc(vendorId).delete()
-}
+  db.collection("vendors").doc(vendorId).delete();
+};
 const deleteProductData = async (productId) => {
-	db.collection('products').doc(productId).delete()
-}
+  db.collection("products").doc(productId).delete();
+};
